b860b2478d02645b1dce30d890bf568141c9ffba
Milosz214234t4 <78423365+Milosz-N@users.noreply.github.com>
2023-01-26 12:00:53 +0100

    v1
    v1

---
+++ src/components/Home.js
@@ -0,0 +1,158 @@
+import React, { useState, useEffect, useRef, useCallback } from "react";
+import Input from "./Input";
+import "../components/scss/fonts.scss";
+function Home() {
+  // var x = 0;
+  const [startGame, setStartGame] = useState(false);
+  const [finish, setFinish] = useState(false);
+  const [timeCounter, setTimeCounter] = useState(false);
+  const [time, setTime] = useState(-3);
+  const intervalIDRef = useRef(null);
+
+  
+  useEffect(() => {
+    // console.log(timeCounter)
+    if(timeCounter == true){
+      startTimer();
+    }
+    else{
+      stopTimer();
+    }
+  }, [timeCounter]);
+ useEffect(() => {
+  document.body.style.backgroundImage =
+      "linear-gradient(90deg, #A6DAF5 36%, #CCF998 100%)";
+    return () => clearInterval(intervalIDRef.current); // to clean up on unmount
+}, []);
+
+  const startTimer = useCallback(() => {
+    intervalIDRef.current = setInterval(() => {
+        setTime(prev => prev + 1);
+    }, 1000);
+}, []);
+
+const stopTimer = useCallback(() => {
+    clearInterval(intervalIDRef.current);
+    intervalIDRef.current = null;
+}, []);
+  const images = importAll(
+    require.context("../components/img", false, /\.(|jpe?g|)$/)
+  );
+  const [selectedImages, setSelectedImages] = useState(
+    new Array(images.length).fill(false)
+  );
+
+  function importAll(r) {
+    return r.keys().map(r);
+  }
+  function isChecked(array) {
+    var map = array.reduce(function (prev, cur) {
+      prev[cur] = (prev[cur] || 0) + 1;
+      return prev;
+    }, {});
+    var x = map.true > 1;
+    return x;
+  }
+
+  const handleCheckboxChange = (position) => {
+    const updatedCheckedState = selectedImages.map((item, index) =>
+      index === position ? !item : item
+    );
+    setSelectedImages(updatedCheckedState);
+  };
+
+  const handleStartGame = (e) => {
+    e.preventDefault();
+    let arr = shuffle(
+      selectedImages.concat(selectedImages).flatMap((b, i) => (b ? i : []))
+    );
+    setSelectedImages(arr);
+    //tutaj podwoic tablice, zrobic flat jak w 63 i wymieszac, taka przekazac na Input
+    setStartGame(true);
+    setTimeCounter(true);
+  };
+  function shuffle(o) {
+    for (
+      var j, x, i = o.length;
+      i;
+      j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x
+    );
+    return o;
+  }
+
+  return (
+    <>
+      {startGame == false && (
+        <>
+          <h2>Choose at least two images</h2>
+          <div className="inputContainer">
+            {images.map((index, x) => (
+              <div
+                className={`${
+                  selectedImages[x] === true ? "inputImage bold" : "inputImage"
+                }`}
+                key={x}
+                style={{
+                  backgroundImage: `url(${require(`../components/img/image-${x}.jpg`)})`,
+                }}
+                onClick={() => {
+                  handleCheckboxChange(x);
+                }} //tu byl x++
+              ></div>
+            ))}
+          </div>
+          <>
+            {isChecked(selectedImages) ? (
+              <label className="toogle-1" onClick={handleStartGame}>
+                <input
+                  type="checkbox"
+                  id="toggle1"
+                  className="toogle-1_input"
+                />
+                <span className="toogle-1_button"></span>
+              </label>
+            ) : (
+              <label className="toogle-2">
+                <input
+                  type="checkbox"
+                  id="toggle2"
+                  className="toogle-2_input"
+                />
+                <span className="toogle-2_button"></span>
+              </label>
+            )}
+          </>
+        </>
+      )}
+      <>
+{startGame == true && <>
+  {time >= 0 == true ? (
+          <>
+            {" "}
+            <Input
+              startGame={startGame}
+              selectedImages={shuffle(selectedImages)}
+              finish={finish}
+              setFinish={setFinish}
+              timeCounter={timeCounter}
+              setTimeCounter={setTimeCounter}
+            
+            />
+            <h2>
+            Czas gry {Math.floor(time / 60)} :{" "}
+                {time - Math.floor(time / 60) * 60}
+            </h2>
+            
+          </>
+        )
+      :<>
+      <h1>{Math.abs(time)}</h1>
+      </>
+      }
+</>} 
+      </>
+    </>
+  );
+}
+
+export default Home;
---
+++ src/components/Input.js
@@ -0,0 +1,159 @@
+import React, { useState, useEffect } from "react";
+import "../App.css";
+import "../components/scss/home.scss";
+import "../components/scss/input.scss";
+function Input({
+  startGame,
+  selectedImages,
+  finish,
+  setFinish,
+  counter,
+  setCounter,
+  timeCounter,
+  setTimeCounter
+}) {
+  const [board, setBoard] = useState([]);
+  const [clicked, setClicked] = useState([]);
+  const [result, setResult] = useState([]);
+  const [dupa, setDupa] = useState(false);
+  //  console.log(selectedImages.length / 2);
+ useEffect(()=> {
+  if(finish === true){
+    console.log('finish hest');
+    setTimeCounter(false);
+  }
+  else{
+    setTimeCounter(true)
+  }
+
+ }, [finish])
+  function rows(x) {
+    if (x < 3) {
+      return { width: "620px", column: "50% 50%" };
+    } else if (x > 2 && x < 5) {
+      return { width: "900px", column: "33% 33% 33%" };
+    } else if (x > 4 && x < 7) {
+      return { width: "1100px", column: "25% 25% 25% 25%" };
+    } else if (x > 6 && x < 11) {
+      return { width: "1300px", column: "20% 20% 20% 20%" };
+    }
+  }
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    // console.log(result);
+    //najpierw wylaczam button, pozniej sprawdzam czy trafilem, jak tak to nic nie zmieniam, jak nie to odblokowuje dwa ostatnie buttony
+    // console.log(e);
+    e.target.disabled = true;
+    clicked.push([e.target.id, e.target.children[0].id]);
+    // console.log(clicked.length%2);
+    e.target.children[0].classList.add("card-animation");
+    // console.log(clicked.length)
+    if (clicked.length > 1) {
+      const firstChecked = document.querySelectorAll(
+        `button[id='${clicked[clicked.length - 1][0]}']`
+      );
+      const secondChecked = document.querySelectorAll(
+        `button[id='${clicked[clicked.length - 2][0]}']`
+      );
+      
+
+
+      if (clicked.length % 2 == 0) {
+        console.log("tutaj bede sprawdzal");
+        // console.log(clicked);
+        // console.log(Number.parseInt(clicked[clicked.length-1][1]) ==  Number.parseInt(clicked[clicked.length-2][1]));
+        if (
+          Number.parseInt(clicked[clicked.length - 1][1]) ==
+          Number.parseInt(clicked[clicked.length - 2][1])
+        ) {
+          console.log("trafilem");
+          // console.log(firstChecked);
+          // console.log(secondChecked);
+          firstChecked[0].classList.add("checked");
+          secondChecked[0].classList.add("checked");
+          result.push(clicked[clicked.length - 1][1]);
+          console.log(Number.parseInt(result.length));
+          console.log(Number.parseInt(board.length)/2);
+          // console.log(board);
+          // console.log(checkedResult)
+          const checkedResult = document.querySelectorAll(
+            `button`
+          );
+            console.log(checkedResult.length)
+
+          if (
+            Number.parseInt(result.length * 2) === Number.parseInt(checkedResult.length)
+          ) {
+            console.log("koniec gry");
+
+            setFinish(true);
+            // setTimeCounter(false)
+
+            // return () => clearInterval(timer);
+          }
+          // a.disabled = 'false';
+          // b.disabled = 'false';
+        }
+      } else {
+        console.log("tutaj bede zerowal");
+        const thirdCheced = document.querySelectorAll(
+          `button[id='${clicked[clicked.length - 3][0]}']`
+        );
+        // console.log(secondChecked[0].classList.contains('checked'));
+        // console.log(thirdCheced[0].classList.contains('checked'));
+        if (secondChecked[0].classList.contains("checked") == false) {
+          secondChecked[0].disabled = false;
+          secondChecked[0].children[0].classList.remove("card-animation");
+        }
+        if (thirdCheced[0].classList.contains("checked") == false) {
+          thirdCheced[0].disabled = false;
+          thirdCheced[0].children[0].classList.remove("card-animation");
+        }
+      }
+    }
+  };
+  for (let x = 0; x < selectedImages.length; x++) {
+    board.push(
+      React.createElement(
+        "button",
+        { className: "card-back", key: x, id: x, onClick: handleClick },
+        [
+          // id diva to numer kontenera, id img to numer obrazka
+          React.createElement("img", {
+            className: "card",
+            src: require(`../components/img/image-${
+              selectedImages[x] > 9 ? selectedImages[x] - 10 : selectedImages[x]
+            }.jpg`),
+            key: x,
+            id:
+              selectedImages[x] > 9
+                ? selectedImages[x] - 10
+                : selectedImages[x],
+            onClick: handleClick,
+          }),
+        ]
+      )
+    );
+  }
+
+  return (
+    <>
+      <div
+        className="cardContainer"
+        style={{
+          maxWidth: `${rows(selectedImages.length / 2).width}`,
+          gridTemplateColumns: `${rows(selectedImages.length / 2).column}`,
+        }}
+      >
+        {board}
+        <>{finish == true && <h2>koniec</h2>}</>
+      </div>
+      <>
+        <h2>{counter}</h2>
+      </>
+    </>
+  );
+}
+
+export default Input;
